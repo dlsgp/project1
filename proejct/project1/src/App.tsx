75780370ec724300e5ce6a608b6995478cf20dc4
dlsgp <khyioh4413@gmail.com>
2024-06-28 14:43:46 +0900

    review end

---
+++ proejct/project1/src/App.tsx
@@ -0,0 +1,13 @@
+import Content from "./components/layout/Content";
+import Layout from "./components/layout/Layout";
+
+
+const App = () => {
+  return (
+    <Layout>
+      <Content />
+    </Layout>
+  );
+};
+
+export default App;
